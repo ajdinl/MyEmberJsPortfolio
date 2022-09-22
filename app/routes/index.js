2137c3020f21ad6be9e67a19a71262a225038f41
Ajdin Lojic <alojic@experfy.com>
2022-09-22 22:19:20 +0200

    upgrade details

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,9 +18,9 @@ export default class IndexRoute extends Route {
         profile:
           'I am a web developer with robust problem-solving skills. Self-taught\n\t\t\t\t\tin React and Node js, with a desire for constant learning and problem-\n\t\t\t\t\tsolving.',
         languages: 'JavaScript, HTML/CSS',
-        more: 'EmberJS, Ember-Data, Next, React, React Router, Redux, Recoil ...',
+        more: 'EmberJS, Ember-Data, NextJS, ReactJS, React Router, Redux',
         other:
-          'Bootstrap, Tailwind CSS, Adobe Photoshop, Linux, Visual Studio Code',
+          'Bootstrap, Tailwind CSS, Adobe XD, Git, Linux, Visual Studio Code',
         personalities: [
           'Accuracy',
           'Organization',
